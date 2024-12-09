e16bfcfa04b291933e1a197bf642a6e71c05a7c8
Victoria Ofuasia <105800461+Solu-victoria@users.noreply.github.com>
2024-12-09 21:58:14 +0100

    Update rockpaperscissorsgame.py

--- rockpaperscissorsgame.py
+++ rockpaperscissorsgame.py
@@ -1,5 +1,43 @@
 import random
+print('''
 
+█▀█ █▀█ █▀▀ █▄▀  
+█▀▄ █▄█ █▄▄ █░█  
+
+█▀█ ▄▀█ █▀█ █▀▀ █▀█  
+█▀▀ █▀█ █▀▀ ██▄ █▀▄  
+
+█▀ █▀▀ █ █▀ █▀ █▀█ █▀█ █▀
+▄█ █▄▄ █ ▄█ ▄█ █▄█ █▀▄ ▄█
+
+''')
+
+rock = ''' 
+       ,--.--._
+------" _, \___)
+        / _/____)
+        \//(____)
+------\     (__)
+       `-----"
+    '''
+paper = '''
+
+           ___..__
+  __..--""" ._ __.'
+              "-..__
+            '"--..__";
+ ___        '--...__"";
+    `-..__ '"---..._;"
+          """"----'    
+'''
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
 computerWins = 0
 userWins = 0
 ties = 0
@@ -26,12 +64,16 @@ while True:
             continue  # Go back to the beginning of the main loop (asking for a new move)
 
     possibleMoves = ['rock', 'paper', 'scissors']
+    imageMoves = [rock, paper, scissors] #ascii art of moves
+
     if userMove not in possibleMoves:
         print('Invalid move! Please enter "rock", "paper", "scissors", or "quit".')
         continue
+    print(imageMoves[possibleMoves.index(userMove)]) #print the corresponding image of the move (find the index of the user's move and use it to locate the corresponding image in imageMoves)
 
     computerMove = random.choice(possibleMoves)
     print(f"My move: {computerMove}", flush=True)
+    print(imageMoves[possibleMoves.index(computerMove)])
 
     if userMove.lower() == computerMove:
         ties += 1
@@ -46,4 +88,4 @@ while True:
     else:
         computerWins += 1
         scoreBoard = f"\nScoreboard:\nComputer: {computerWins}\t\t\tYou: {userWins}\t\t\tTies: {ties}"
-        print(f"Computer wins! {scoreBoard}", flush=True)
+        print(f"You lose! {scoreBoard}", flush=True)
